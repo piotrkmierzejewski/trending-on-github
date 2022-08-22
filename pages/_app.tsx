c0b97896a2a6dc04abeb1ac4fb6d7820a1e381e6
Piotr Mierzejewski <piotr@mailpencil.app>
2022-08-22 19:10:36 +0200

    Add MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material'
+import { theme } from '../helpers/theme'
+import createEmotionCache from '../helpers/createEmotionCache'
+import { CacheProvider } from '@emotion/react'
+
+const clientSideEmotionCache = createEmotionCache()
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CacheProvider value={clientSideEmotionCache}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
